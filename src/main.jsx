5fda1bfcbddc166ea3ee60d8da3013be67a3876d
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-23 16:22:50 +0500

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Home from "./assets/Home.jsx";
+import Home from "./Components/home/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
