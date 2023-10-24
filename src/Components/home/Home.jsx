3bf159b827bb5e356e4a3660db428010b5e3dc25
SU <valej200554@gmail.com>
2023-10-24 14:41:25 +0500

    add

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react"
-import "./home-components/help/Help"
-import Help from "./home-components/help/Help"
-import Header from "../global-components/header/header"
-import Main from "./home-components/main/Main"
+import { useState } from "react";
+import "./home-components/help/Help";
+import Help from "./home-components/help/Help";
+import Header from "../global-components/header/header";
+import Main from "./home-components/main/Main";
+import Advantages from "./home-components/advantages/advantages";
 
 const Home = () => {
   return (
     <div>
-      <div style={{ backgroundImage: `url(/public/source/mainimage.png)`, backgroundSize: 'cover', }}>
+      <div
+        style={{
+          backgroundImage: `url(/public/source/mainimage.png)`,
+          backgroundSize: "cover",
+        }}
+      >
         <Header color="white" />
         <Main />
       </div>
@@ -16,10 +22,10 @@ const Home = () => {
           paddingLeft: 100,
         }}
       >
+        <Advantages />
         <Help />
       </div>
     </div>
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
