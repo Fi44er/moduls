222eb491f9f92fc2c908e947a8671174abdfdbfb
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-24 12:33:24 +0300

    add

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,32 +1,12 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import "./home-components/help/Help";
-import Help from "./home-components/help/Help";
-import Header from "../global-components/header/Header";
-import Main from "./home-components/main/Main";
-import Advantages from "./home-components/advantages/advantages";
-=======
 import { useState } from "react"
 import "./home-components/help/Help"
 import Help from "./home-components/help/Help"
 import Header from "../global-components/header/header"
 import Main from "./home-components/main/Main"
->>>>>>> 23b897caa9bdad48b092b1668811fd4c1f9ea3fb
 
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <div style={{ backgroundImage: `url(/public/source/mainimage.png)` }}>
-        <Header />
-        <Main />
-      </div>
-      <Advantages />
-      <Help />
-    </div>
-  );
-};
-=======
       <div style={{ backgroundImage: `url(/public/source/mainimage.png)`, backgroundSize: 'cover', }}>
         <Header color="white" />
         <Main />
@@ -41,6 +21,5 @@ const Home = () => {
     </div>
   )
 }
->>>>>>> 23b897caa9bdad48b092b1668811fd4c1f9ea3fb
 
-export default Home;
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client"
 import "./global.css"
 import Home from "./Components/home/Home.jsx"
 
-<<<<<<< HEAD
-ReactDOM.createRoot(document.getElementById("root")).render(<Home />);
-=======
 ReactDOM.createRoot(document.getElementById("root")).render(<Home />)
->>>>>>> 23b897caa9bdad48b092b1668811fd4c1f9ea3fb
