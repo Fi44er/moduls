2a373376113b16b6e99eb0f37d48263eafbc52b6
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-24 11:13:34 +0300

    add

--- src/Components/global-components/header/header.jsx
+++ src/Components/global-components/header/header.jsx
@@ -1,59 +1,48 @@
+import Button from "../button/Button"
 import style from "./Header.module.css"
 
-function Header () {
-    return (
-        
-        <header className={style.headerBlock}>
-            <div className={style.headerItems}>
-                <div className={style.headerMenu}>
-                    <button>
-                        <img src="/public/source/icon/menu.png" alt="menu" />
-                    </button>
-                </div>
-                <div className={style.headerLogo}>
-                    <a href="">
-                        <div>
-                            <h3>
-                                RosLogistic
-                            </h3>
-                            <p>
-                                Логистическая<br></br> компания
-                            </p>
-                        </div>
-                    </a>
-                </div>
-                <div className={style.headerLang}>
-                    <button>
-                        RU
-                    </button>
-                    <button>
-                        EN
-                    </button>
-                    <button>
-                        字
-                    </button>
-                </div>
-                <div className={style.headerNumber}>
-                    <a href="">
-                        <h3>8 000 000 00 00</h3>
-                        <p>Федеральный</p>
-                    </a>
-                    <a href="">
-                        <h3>8 000 000 00 00</h3>
-                        <p>Санкт-Петербург</p>
-                    </a>
-                </div>
-                <div className={style.lookPrice}>
-                    <button>
-                        <div>
-                        <p>Рассчитать стоимость</p>
-                        <img src="/public/source/icon/arrow-mini-white.png" alt="" />
-                        </div>
-                    </button>
-                </div>
+function Header({ color }) {
+  return (
+    <header className={style.headerBlock}>
+      <div className={style.headerItems}>
+        <div className={style.headerMenu}>
+          <button>
+            <img src="/public/source/icon/menu.png" alt="menu" />
+          </button>
+        </div>
+        <div className={style.headerLogo}>
+          <a href="!#" style={{ color: color }}>
+            <div>
+              <h3 style={{ color: color }}>RosLogistic</h3>
+              <p style={{ color: color }}>
+                Логистическая<br></br> компания
+              </p>
             </div>
-         </header>
-    )
+          </a>
+        </div>
+        <div className={style.headerLang}>
+          <button style={{ color: color }}>RU</button>
+          <button style={{ color: color }}>EN</button>
+          <button style={{ color: color }}>字</button>
+        </div>
+        <div className={style.headerNumber}>
+          <a href="">
+            <h3 style={{ color: color }}>8 000 000 00 00</h3>
+            <p style={{ color: color }}>Федеральный</p>
+          </a>
+          <a href="">
+            <h3 style={{ color: color }}>8 000 000 00 00</h3>
+            <p style={{ color: color }}>Санкт-Петербург</p>
+          </a>
+        </div>
+        <div className={style.lookPrice}>
+          <Button backBtn="white" buttonImg="black">
+            Рассчитать стоимость
+          </Button>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,23 +1,18 @@
 import { useState } from "react"
 import "./home-components/help/Help"
 import Help from "./home-components/help/Help"
-import Header from '../global-components/header/Header'
-import Main from './home-components/main/Main'
+import Header from "../global-components/header/header"
+import Main from "./home-components/main/Main"
 
 const Home = () => {
   return (
     <div>
-      <div style={{backgroundImage:`url(/public/source/mainimage.png)`}}>
-        <Header />
-        <Main/>
-       
-      </div> 
+      <div style={{ backgroundImage: `url(/public/source/mainimage.png)` }}>
+        <Header color="white" />
+        <Main />
+      </div>
       <Help />
     </div>
-     
-    
-    
-   
   )
 }
 
